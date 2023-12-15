1cb1aba8fdb57cb33518e9ba7912bb5af73708af
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-12-14 23:07:09 -0300

    To programmers version

--- src/main/java/br/edu/unilab/unicaffe/registro/model/Perfil.java
+++ src/main/java/br/edu/unilab/unicaffe/registro/model/Perfil.java
@@ -134,6 +134,16 @@ public class Perfil {
 	 */
 	public static Collection<Registro> listaParaBloqueio() {
 		Collection<Registro> lista = new HashSet<Registro>();
+
+
+		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoRun",
+				Registro.REG_DWORD, "0", "0", "Não abrir o Executar"));
+		lista.add(new Registro("HKCU\\Software\\Policies\\Microsoft\\Windows\\System", "DisableCMD", Registro.REG_DWORD,
+				"0", "0", "Desativar prompt"));
+		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\DisallowRun",
+				"9", Registro.REG_SZ, "", "", "cmd"));
+		// Coisas que vou setar como default Pensando nos programadores.
+
 		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoClose",
 				Registro.REG_DWORD, "1", "0", "Sem o Botão de Desligar no Menu Iniciar"));
 		lista.add(new Registro("HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoClose",
@@ -145,77 +155,62 @@ public class Perfil {
 				"Remove Opção para Trocar de Usuário no Ctrl+ALT+DEL"));
 		lista.add(new Registro("HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",
 				"DisableChangePassword", Registro.REG_DWORD, "1", "0", "Remove Opção Trocar Senha no Ctrl+ALt+DEl"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr",
-				Registro.REG_DWORD, "1", "0", "Remove Opção para Iniciar o Gerenciador de Tarefas no Ctrl+ALT+DEL"));
 		lista.add(new Registro("HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",
 				"DisableLockWorkstation", Registro.REG_DWORD, "1", "0",
 				"Remove Opção para Bloquear este Computador no Ctrl+ALT+DEL"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "Wallpaper",
-				Registro.REG_SZ, "C:\\Windows\\Web\\Wallpaper\\Windows\\papel-de-parede.jpg",
-				"C:\\Windows\\Web\\Wallpaper\\Windows\\img0.jpg", "definir papel de parede"));
+		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
+				"NoStartMenuMFUprogramsList", Registro.REG_DWORD, "1", "0", "Sem lista de programas no Menu Iniciar"));
+
+		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr",
+				Registro.REG_DWORD, "1", "0", "Remove Opção para Iniciar o Gerenciador de Tarefas no Ctrl+ALT+DEL"));
 		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
 				"NoControlPanel", Registro.REG_DWORD, "1", "0", "Desabilita Painel de Controle"));
-		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoRun",
-				Registro.REG_DWORD, "1", "0", "Não abrir o Executar"));
 		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Programs",
 				"NoProgramsAndFeatures", Registro.REG_DWORD, "1", "0",
 				"Não exibir a lista no Add ou remover programas"));
-		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoDrives",
-				Registro.REG_DWORD, "4", "0", "Não pode ver o disco C:"));
 		lista.add(new Registro("HKLM\\SOFTWARE\\Policies\\Microsoft\\Internet Explorer\\Restrictions", "NoFavorites",
 				Registro.REG_DWORD, "1", "0", "Não ver Favoritos do Meu Computador"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
-				"NoStartMenuMFUprogramsList", Registro.REG_DWORD, "1", "0", "Sem lista de programas no Menu Iniciar"));
 		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr",
 				Registro.REG_DWORD, "1", "0", "Desativar Gerenciador de Tarefas"));
 		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
-				"NoPropertiesMyComputer", Registro.REG_DWORD, "1", "0", "Não ver Propriedades do Meu Computador"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
 				"NoFolderOptions", Registro.REG_DWORD, "1", "0", "Remove Opções de Pastas"));
-		lista.add(new Registro("HKCU\\Software\\Policies\\Microsoft\\Windows\\System", "DisableCMD", Registro.REG_DWORD,
-				"1", "0", "Desativar prompt"));
-		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update",
-				"AUOptions", Registro.REG_DWORD, "1", "4", "Desativar Opções Update"));
-		lista.add(new Registro("HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU", "NoAutoUpdate",
-				Registro.REG_DWORD, "1", "0", "Desativar Update"));
+		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
+				"NoPropertiesMyComputer", Registro.REG_DWORD, "1", "0", "Não ver Propriedades do Meu Computador"));
+		lista.add(new Registro("HKLM\\SOFTWARE\\Classes\\Wow6432Node\\CLSID\\{323CA680-C24D-4099-B94D-446DD2D7249E}",
+				"ShellFolder", Registro.REG_DWORD, "a9400100", "a0900100", "Não ver Propriedades do Meu Computador"));
+		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoDrives",
+				Registro.REG_DWORD, "4", "0", "Não pode ver o disco C:"));
+		// Relacionado ao wallpaper
+
+		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "Wallpaper",
+				Registro.REG_SZ, "C:\\Windows\\Web\\Wallpaper\\Windows\\papel-de-parede.jpg",
+				"C:\\Windows\\Web\\Wallpaper\\Windows\\img0.jpg", "definir papel de parede"));
 		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "WallpaperStyle",
 				Registro.REG_DWORD, "2", "0", "impede a alteração do plano de fundo"));
 		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\ActiveDesktop",
 				"NoChangingWallpaper", Registro.REG_DWORD, "1", "0", "Não altera plano de fundo"));
-		lista.add(new Registro(
-				"HKCU\\Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\CurrentVersion\\TrayNotify",
-				"PastIconsStream", Registro.REG_DWORD, "0", "0", "Mostra os icones na area de notificação"));
-		lista.add(new Registro(
-				"HKCU\\Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\CurrentVersion\\TrayNotify",
-				"IconStreams", Registro.REG_DWORD, "0", "0", "Mostra os icones na area de notificação"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer", "EnableAutoTray",
-				Registro.REG_DWORD, "0", "0", "Mostra os icones na area de notificação"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
-				"NoTrayItemsDisplay", Registro.REG_DWORD, "0", "0", "Mostra os icones na area de notificação"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "Hidden",
-				Registro.REG_DWORD, "0", "0", "Ocultar arquivos de sistema"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced",
-				"ShowSuperHidden", Registro.REG_DWORD, "0", "0", "Ocultar arquivos de sistema protegido"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "HideFileExt",
-				Registro.REG_DWORD, "1", "1", "Ocultar extensões"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "DisallowRun",
-				Registro.REG_DWORD, "1", "0", "Bloqueio de Programas"));
-		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\DisallowRun",
-				"9", Registro.REG_SZ, "cmd.exe", "", "cmd"));
+
+		// Proteção de tela
 		lista.add(new Registro("HKCU\\Control Panel\\Desktop", "SCRNSAVE.EXE", Registro.REG_SZ,
 				"C:\\projetos\\unicafe\\projeto\\unicafeWin\\unicafe\\src\\main\\resources\\images\\UniCafe.scr",
 				"C:\\Windows\\system32\\PhotoScreensaver.scr", "Local da proteção de tela"));
-		lista.add(new Registro("HKCU\\Control Panel\\Desktop", "ScreenSaveTimeOut", Registro.REG_SZ, "60", "60",
-				"Determina o tempo"));
 		lista.add(new Registro("HKCU\\Control Panel\\Desktop", "ScreenSaveActive", Registro.REG_SZ, "1", "1",
 				"Ativa proteção de tela"));
 		lista.add(new Registro("HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer",
 				"StartMenuLogoff", Registro.REG_DWORD, "1", "1", "Ativa proteção de tela"));
-		lista.add(new Registro("HKLM\\SOFTWARE\\Classes\\Wow6432Node\\CLSID\\{323CA680-C24D-4099-B94D-446DD2D7249E}",
-				"ShellFolder", Registro.REG_DWORD, "a9400100", "a0900100", "Não ver Propriedades do Meu Computador"));
-		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",
-				"ConsentPromptBehaviorAdmin", Registro.REG_DWORD, "00", "00",
-				"Não perguntar se pode abrir coisas como admin"));
+
+		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update",
+				"AUOptions", Registro.REG_DWORD, "1", "4", "Desativar Opções Update"));
+		lista.add(new Registro("HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU", "NoAutoUpdate",
+				Registro.REG_DWORD, "1", "0", "Desativar Update"));
+
+		lista.add(new Registro("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "DisallowRun",
+				Registro.REG_DWORD, "1", "0", "Bloqueio de Programas"));
+
+		lista.add(new Registro("HKCU\\Control Panel\\Desktop", "ScreenSaveTimeOut", Registro.REG_SZ, "60", "60",
+				"Determina o tempo"));
+
+
 		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon", "DefaultUserName",
 				Registro.REG_SZ, ".\\unicafe", ".\\unicafe", "Login default de usuario do SO"));
 		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon", "DefaultPassword",
@@ -227,6 +222,13 @@ public class Perfil {
 		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce", "unicafe",
 				Registro.REG_SZ, "C:\\Program Files\\UniCafe\\UniCafeClient.exe",
 				"C:\\Program Files\\UniCafe\\UniCafeClient.exe", "inicia Unicaffe"));
+
+
+
+		lista.add(new Registro("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",
+				"ConsentPromptBehaviorAdmin", Registro.REG_DWORD, "00", "00",
+				"Não perguntar se pode abrir coisas como admin"));
+
 		return lista;
 	}
 
